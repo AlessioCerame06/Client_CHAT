1b2f5503ad4f6794a84181e7a4349cdb45880c67
lai2-24 <lai2-24@itismeucci.com>
2024-11-11 13:45:26 +0100

    commit 2, login quasi finito

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,37 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket socket = new Socket("localhost", 10100);
+        Scanner scanner = new Scanner(System.in);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        //login
+        System.out.println("Digita Login <nome utente> per iniziare");
+        String utente = " ";
+        String esitoLogin = " " ;
+
+        do{
+            utente = scanner.nextLine();
+            out.writeBytes(utente + "\n");
+            esitoLogin = in.readLine();
+        }while(esitoLogin.equals("login failed"));
+        
+        
+        
+
+        do {
+            
+        } while (true);
     }
 }
