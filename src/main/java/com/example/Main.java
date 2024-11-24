2a730cde414fe040cc9ca10826a4eafec26bef79
AlessioCerame06 <alessiocerame@gmail.com>
2024-11-24 18:42:01 +0100

    Client aggiornato

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,84 +3,51 @@
 package com.example;
 
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        try {
-            Socket socket = new Socket("localhost", 10100);
-            MyThread clientThread = new MyThread(socket);
-            clientThread.start();
-            Scanner scanner = new Scanner(System.in);
-
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 10100);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            boolean statoLogin = false;
+            Scanner scanner = new Scanner(System.in)) {
 
             String comando = "";
-            do {
-                System.out.println("\n");
-                System.out.println("digita LOGIN <nome utente> per accedere");
-                System.out.println("digita MSG <destinatario> <messaggio> per inviare un messaggio privato");
-                System.out.println("digita MSG ALL <messaggio> per inviare un messaggio a tutti");
-                System.out.println("digita USERS per ricevere la lista degli utenti connessi");
-                System.out.println("digita LOGOUT per disconnetterti");
-                System.out.println("\n");
-
+            while (true) {
+                System.out.println("Comandi: LOGIN <nome>, USERS, MSG <destinatario> <messaggio>, MSG ALL <messaggio>, LOGOUT");
                 comando = scanner.nextLine();
-                String[] elementiComando = comando.split(" ", 2);
-                String esito = " ";
+                out.writeBytes(comando + "\n");
+                String risposta = in.readLine();
 
-                if (elementiComando[0].equals("LOGIN") && statoLogin == false) {
-                    out.writeBytes(comando + "\n");
-                    esito = in.readLine();
-                    if (esito.contains("OK LOGIN")) {
-                        System.out.println("Login avvenuto con successo");
-                    } else {
-                        System.out.println("Errore LOGIN");
-                    }
-                    statoLogin = true;
-                } else if (elementiComando[0].equals("USERS") && statoLogin == true) {
-                    out.writeBytes(comando);
-                    String risposta = in.readLine();
-                    if (risposta.contains("OK USERS")) {
-                        String listaElementi[] = risposta.split(" ", 3);
-                        String listaUtenti[] = listaElementi[2].split(",");
-                        System.out.println("Lista utenti:");
-                        for (int i = 0; i < listaUtenti.length; i++) {
-                            System.out.println(listaUtenti[i]);
-                        }
-                    } else {
-                        System.out.println("Errore nella richiesta");
-                    }
-                } else if (elementiComando[0].equals("MSG") && statoLogin == true) {
-                    out.writeBytes(comando);
-                    esito = in.readLine();
-                    if (esito.contains("ERROR MSG")) {
-                        System.out.println("Messaggio non inviato");
-                    }
-                } else if (comando.contains("MSG ALL")) {
-                    out.writeBytes(comando);
-                    esito = in.readLine();
-                    if (esito.contains("ERROR MSG ALL")) {
-                        System.out.println("Messaggio non inviato");
-                    }
-                } else if (comando.contains("LOGOUT")) {
+                if (comando.startsWith("LOGOUT")) {
                     break;
-                } else {
-                    System.out.println("Errore. Digitare un comando esistente");
+                } else if (risposta.contains("OK LOGIN")) {
+                    System.out.println("Login effetuato");
+                } else if (risposta.equals("ERROR LOGIN username not valid or already used")) {
+                    System.out.println("Errore. Username non valido o è già stato inserito");
+                } else if (risposta.equals("ERROR you must login first")) {
+                    System.out.println("Per prima cosa di devi loggare");
+                } else if (risposta.contains("OK USERS")) {
+                    System.out.println("Ecco la lista degli utenti:");
+                    String stringaUtenti = risposta.substring(9);
+                    String listaUtenti[] = stringaUtenti.split(",");
+                    for (String utente : listaUtenti) {
+                        System.out.println(utente);
+                    }
+                } else if (risposta.equals("ERROR MSG invalid format")) {
+                    System.out.println("Errore. Formato non valido");
+                } else if (risposta.equals("ERROR MSG user not found")) {
+                    System.out.println("Errore. Utente non trovato");
+                } else if (risposta.equals("ERROR command not recognized")) {
+                    System.out.println("Inserire un comando esistente");
+                } else if (!risposta.equals("OK MSG PRIVATE") && !risposta.equals("OK LOGOUT") && !risposta.equals("OK MSG ALL")) {
+                    String[] partiMessaggio = risposta.split(" ", 4);
+                    System.out.println("Da " + partiMessaggio[1] + ": " + partiMessaggio[3]);
                 }
-            } while (true);
-
-            socket.close();
-            scanner.close();
-            in.close();
-            out.close();
-            System.out.println("Sei stato disconesso");
-        } catch (IOException e) {
-            System.out.println("Errore durante la connessione al server: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            System.out.println("Errore client: " + e.getMessage());
         }
     }
 }
